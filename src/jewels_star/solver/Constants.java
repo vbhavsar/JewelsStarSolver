d972863c859f09c859b8301d89154dc662e9a804
vishal <vbhavsar@gmail.com>
2014-06-01 17:39:24 -0400

    Finally got the dimensions of the cell right.

--- src/jewels_star/solver/Constants.java
+++ src/jewels_star/solver/Constants.java
@@ -7,11 +7,11 @@ package jewels_star.solver;
  */
 public class Constants {
 
-    public static int CELL_WIDTH = 75;
-    public static int CELL_HEIGHT = 75;
+    public static int CELL_WIDTH = 87;
+    public static int CELL_HEIGHT = 80;
 
     public static int CELL_ROWS = 9;
     public static int CELL_COLS = 7;
 
-    public static int BUFFER = 25;
+    public static int BUFFER = 5;
 }
