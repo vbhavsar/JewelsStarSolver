cd9a81efbe778c0ff7caa290d24342e707d6fdf9
vishal <vbhavsar@gmail.com>
2014-06-30 22:41:06 -0400

    Solver works somewhat

---
+++ src/main/java/jewels_star/ml/KMeans.java
@@ -0,0 +1,66 @@
+package jewels_star.ml;
+
+import jewels_star.solver.Cell;
+import jewels_star.solver.Grid;
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.core.DenseInstance;
+import net.sf.javaml.core.Instance;
+
+import java.util.*;
+
+import static jewels_star.solver.Constants.*;
+
+/**
+ * User: vishal
+ * Date: 6/29/14
+ * Time: 3:02 PM
+ */
+public class KMeans {
+
+    private final Grid grid;
+
+    public KMeans(final Grid grid) {
+        this.grid = grid;
+    }
+
+    public static Map<String, Integer> classify(Cell[][] cells) {
+
+        final Collection<Instance> data = new ArrayList<Instance>();
+
+        for (int row=0; row<cells.length; row++) {
+            for (int col=0; col<cells[row].length; col++) {
+                final Cell cell = cells[row][col];
+                final int[] rgb = cell.getRgb();
+
+                DenseInstance inst = new DenseInstance(3);
+                inst.put(R, Double.valueOf(rgb[0]));
+                inst.put(G, Double.valueOf(rgb[1]));
+                inst.put(B, Double.valueOf(rgb[2]));
+
+                data.add(inst);
+            }
+        }
+
+        net.sf.javaml.clustering.KMeans kmeans = new net.sf.javaml.clustering.KMeans(7, 100);
+        Dataset dataset = new DefaultDataset(data);
+        final Dataset[] clusters = kmeans.cluster(dataset);
+
+        Map<String, Integer> rgbToClassMap = new HashMap<String, Integer>();
+
+        for (int icluster = 0; icluster < clusters.length; icluster++) {
+            final Iterator<Instance> iterator = clusters[icluster].iterator();
+            while(iterator.hasNext()) {
+                final Instance inst = iterator.next();
+                int r = inst.get(R).intValue();
+                int g = inst.get(G).intValue();
+                int b = inst.get(B).intValue();
+
+                final String rgb = Cell.getRGB(r, g, b);
+                rgbToClassMap.put(rgb, icluster);
+            }
+        }
+
+        return rgbToClassMap;
+    }
+}
---
+++ src/main/java/jewels_star/solver/Direction.java
@@ -0,0 +1,10 @@
+package jewels_star.solver;
+
+/**
+ * User: vishal
+ * Date: 6/30/14
+ * Time: 9:53 PM
+ */
+public enum Direction {
+    NORTH, SOUTH, EAST, WEST;
+}
